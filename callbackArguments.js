9528c651897602edd9fe7de195b97c726e8a1e1d
SebastianPez <Sebastian.Pezarro@gmail.com>
2019-03-07 20:10:38 +0000

    changed to forEach

--- callbackArguments.js
+++ callbackArguments.js
@@ -1,15 +1,25 @@
 // The second argument/parameter is expected to be a function
-function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(arr[i], i);   // execute callback
-    }
-  }
-}
+// function findWaldo(arr, found) {
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] === "Waldo") {
+//       found(arr[i], i);   // execute callback
+//     }
+//   }
+// }
 
-function actionWhenFound(arr, index) {
-  console.log("Found " + arr + " at index " + index + "!");
+// function actionWhenFound(arr, index) {
+//   console.log("Found " + arr + " at index " + index + "!");
   // console.log("Found him!");
-}
+// }
 
-findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+// findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+var names = ["Alice", "Bob", "Waldo", "Winston"];
+
+var actionWhenFound = function(name, i) {
+  if (name === "Waldo"){
+  console.log("Found " + name + " at index " + i + "!");
+  }
+};
+
+names.forEach(actionWhenFound);
